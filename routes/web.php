c0da7180b86eb4e5daa7c365dd6847204bd6aad8
neo <neomn110@gmail.com>
2022-03-11 07:01:57 +0330

    route articles and about created with thier views

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('article', function (){
+   return view('articles');
+});
+
+Route::get('about', function (){
+    return view('about');
+});
