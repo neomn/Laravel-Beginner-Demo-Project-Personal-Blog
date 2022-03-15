fe7c3fb35a42bd27710cb53566d6aac74e7f3cee
neo <neomn110@gmail.com>
2022-03-15 15:04:37 +0330

    articles management tab added to the dashboard page

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,7 +16,7 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('Articles')">
-                        <h3>Articles Management</h3>
+                        <h3>Articles Management </h3>
                     </x-nav-link>
                 </div>
             </div>
