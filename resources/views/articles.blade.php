980267dd3d7948e10753125b7d1b533e00eeb335
neo <neomn110@gmail.com>
2022-03-16 13:42:24 +0330

    some changes applied to the login page dark theme

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -7,5 +7,25 @@
         </x-slot>
 
         <h1 class="p-6">this is articles page </h1>
+
+        <div>
+            <table class="text-center ">
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">title</th>
+                    <th scope="col">date</th>
+                </tr>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>test title</td>
+                    <td>2022/3/15</td>
+                </tr>
+                <tr>
+                    <td> record 2</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </x-app-layout>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,11 +19,11 @@
             <div>
                 <x-label for="email" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-input id="email" class="text-gray-900 block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <!-- Password -->
-            <div class="mt-4">
+            <div class="mt-4 text-gray-900">
                 <x-label for="password" :value="__('Password')" />
 
                 <x-input id="password" class="block mt-1 w-full"
@@ -35,7 +35,7 @@
             <!-- Remember Me -->
             <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-gray-200 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 bg-slate-700 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
                     <span class="ml-2 text-sm text-gray-200">{{ __('Remember me') }}</span>
                 </label>
             </div>
