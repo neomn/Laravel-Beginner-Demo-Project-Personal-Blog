518cf923f749cba755951c7c0bdaed46ad161997
neo <neomn110@gmail.com>
2022-03-16 22:57:47 +0330

    articles table style changed

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -9,22 +9,27 @@
         <h1 class="p-6">this is articles page </h1>
 
         <div class=" px-4  inline-flex  table-fixed">
-            <table class="text-center   border-separate border border-gray-500">
+            <table class="text-center   border-separate border-2 p-4 rounded border-gray-500">
                 <thead class="border-b">
                 <tr>
-                    <th scope="col" class="px-4">ID</th>
-                    <th scope="col">title</th>
-                    <th scope="col">content</th>
-                    <th scope="col">date</th>
+                    <th scope="col" class="px-3 border-b">ID</th>
+                    <th scope="col" class="px-3 border-b">title</th>
+                    <th scope="col" class="px-3 border-b">content</th>
+                    <th scope="col" class="px-3 border-b">date</th>
                 </tr>
                 </thead>
                 <tbody>
                     @foreach($article as $article)
                         <tr>
-                            <td>{{$loop->iteration}}</td>
-                            <td>{{$article->title}}</td>
-                            <td>{{$article->content}}</td>
-                            <td>{{$article->created_at}}</td>
+                            <td class="px-3">{{$loop->iteration}}</td>
+                            <td class="px-3">{{$article->title}}</td>
+                            <td class="px-3">{{$article->content}}</td>
+                            <td class="px-3">{{$article->created_at}}</td>
+                            <td>
+                                <form action="post">
+                                    <button type="submit" class="px-4 mx-4 border rounded bg-red-900 "> delete </button>
+                                </form>
+                            </td>
                         </tr>
                     @endforeach
                 </tbody>
